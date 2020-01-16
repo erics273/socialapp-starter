62b269fd2603550635fb32399db32dda616d5c37
Alex Zimdahl <azimdahl91@gmail.com>
2020-01-16 16:40:20 -0500

    create endpoints to use

--- src/socialAppService.js
+++ src/socialAppService.js
@@ -2,16 +2,60 @@
 import axios from "axios";
 
 class SocialAppService {
-    constructor(url = "https://socialapp-api.herokuapp.com", client = axios.create()) {
+    constructor(url = "https://socialapp-api.herokuapp.com/", client = axios.create()) {
         this.url = url;
         this.endpoint = {
-            newUser : "/users"
+            users: "users/",
+            messages: "messages/",
+            likes: "likes/"
         }
         this.client = client;
     }
 
+    /*User functions*/
     createNewUser(credentials) {
-        return this.client.post(this.url + this.endpoint.newUser, credentials)
+        return this.client.post(this.url + this.endpoint.users, credentials)
+    }
+
+    deleteUser(username) {
+        return this.client.delete(this.url + this.endpoint.users + username)
+    }
+
+    updateUser(username, credentials) {
+        return this.client.push(this.url + this.endpoint.users + username, credentials)
+    }
+
+    //PICTURE DATA SIZE IS RESTRICTED TO <= 200kb
+    setUserPicture(username, picture) {
+        return this.client.put(this.url + this.endpoint.users + username, picture)
+    }
+
+    getUsersList(limit) {
+        return this.client.get(this.url + this.endpoint.users)
+    }
+
+
+    /*Message functions */
+    createMessage(content) {
+        return this.client.post(this.url + this.endpoint.messages, content)
+    }
+
+    getMessagesList(limit, username) {
+        return this.client.get(this.url + this.endpoint.messages)
+    }
+
+    deleteMessage(messageId) {
+        return this.client.delete(this.url + this.endpoint.messages, messageId)
+    }
+
+
+    /*Like functions*/
+    addLike(messageId) {
+        return this.client.post(this.url + this.endpoint.likes, messageId)
+    }
+
+    deleteLike(likeId) {
+        return this.client.delete(this.url + this.endpoint.likes, likeId)
     }
 }
 
