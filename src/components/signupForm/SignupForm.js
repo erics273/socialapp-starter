6149eb3a2882a33b6eb18b714b424b6247555da3
Alex Zimdahl <azimdahl91@gmail.com>
2020-01-16 14:28:56 -0500

    built register

--- src/components/signupForm/SignupForm.js
+++ src/components/signupForm/SignupForm.js
@@ -2,34 +2,60 @@ import React, { Component } from "react"
 import "./SignupForm.css"
 import TextField from '@material-ui/core/TextField';
 
+//import our service
+import SocialAppService from "../../socialAppService";
+
 class SignupForm extends Component {
     constructor(props) {
         super(props)
+        this.client = new SocialAppService;
+        this.state = {
+            formData: {
+                username: "",
+                displayName: "",
+                password: ""
+            }
+        }
+    }
+
+    handleChange = (event) => {
+        let formData = this.state.formData;
+        formData[event.target.name] = event.target.value;
+        this.setState({formData});
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        return this.client.createNewUser(this.state.formData)
     }
 
     render() {
         return (
-            <form>
+            <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
                 <TextField
-          required
-          id="outlined-required"
-          label="Username"
-          variant="outlined"
-        />
+                    required
+                    id="outlined-required"
+                    name="username"
+                    label="Username"
+                    variant="outlined"
+                />
                 <TextField
-          required
-          id="outlined-required"
-          label="Display Name"
-          variant="outlined"
-        />
+                    required
+                    id="outlined-required"
+                    name="displayName"
+                    label="Display Name"
+                    variant="outlined"
+                />
                 <TextField
                     required
                     id="outlined-password-input"
+                    name="password"
                     label="Password"
                     type="password"
                     autoComplete="current-password"
                     variant="outlined"
                 />
+                <button>Submit</button>
             </form>
         )
     }
