b27cd24afe5b2a875cb83dfde0555becff98d172
Mullen <mullensh@amazon.com>
2020-01-21 10:17:51 -0800

    changed user info component from simple card to media card

--- src/components/userInfo/UserInfo.js
+++ src/components/userInfo/UserInfo.js
@@ -1,56 +1,51 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   card: {
-    minWidth: 275,
+    maxWidth: 345,
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
+  media: {
+    height: 140,
   },
 });
 
-export default function SimpleCard() {
+export default function MediaCard() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.card}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          User
-       </Typography>
-        <Typography variant="h5" component="h2">
-          First name Last name
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Age
-        </Typography>
-        <Typography variant="body2" component="p">
-          About
-          <br />
-          {'"a creative person"'}
-        </Typography>
-      </CardContent>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
       <CardActions>
-         <Button size="small">Add Image</Button>
-        <Button size="small">Update</Button>
-
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
       </CardActions>
-
     </Card>
   );
 }
