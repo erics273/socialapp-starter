9d3cd2afa5f2bce2e1e3111daf7316a6b803f976
Alex Zimdahl <azimdahl91@gmail.com>
2020-01-23 13:20:34 -0500

    UserInfo from stateless to stateful

--- src/components/userInfo/UserInfo.js
+++ src/components/userInfo/UserInfo.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withAsyncAction } from "../../HOCs";
+
+import SocialAppService from "../../socialAppService";
+
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { withAsyncAction } from "../../HOCs";
+import { withStyles } from '@material-ui/core/styles';
 
 
-import SocialAppService from "../../socialAppService";
-
-const useStyles = makeStyles({
+const useStyles = makeStyles => ({
   card: {
     minWidth: 275,
   },
@@ -27,47 +29,57 @@ const useStyles = makeStyles({
   },
 });
 
+class UserInfo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      updateUser: false,
 
-
-function UserInfo(props) {
-
-  const handleDelete = event => {
-    const client = new SocialAppService()
-    client.deleteUser(props.username)
-    props.logout()
+    }
   }
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
-  return (
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          User
+  handleDelete = event => {
+    const client = new SocialAppService()
+    client.deleteUser(this.props.username)
+    this.props.logout()
+  }
+
+  handleUpdate() {
+
+  }
+
+  render() {
+    const { classes } = this.props;
+    const bull = <span className={classes.bullet}>•</span>;
+
+    return (
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            User
        </Typography>
-        <Typography variant="h5" component="h2">
-          First name Last name
+          <Typography variant="h5" component="h2">
+            First name Last name
         </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Age
+          <Typography className={classes.pos} color="textSecondary">
+            Age
         </Typography>
-        <Typography variant="body2" component="p">
-          About
+          <Typography variant="body2" component="p">
+            About
           <br />
-          {'"a creative person"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Add Image</Button>
-        <Button size="small">Update</Button>
-        <Button onClick={handleDelete} size="small">Delete</Button>
-
-      </CardActions>
-
-    </Card>
-  );
+            {'"a creative person"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Add Image</Button>
+          <Button size="small">Update</Button>
+          <Button onClick={this.handleDelete} size="small">Delete</Button>
+        </CardActions>
+      </Card>
+    );
+  }
 }
 
 const connectedUser = withAsyncAction("auth", "logout")(UserInfo)
 
-export default connectedUser
+export default withStyles(useStyles)(connectedUser)
