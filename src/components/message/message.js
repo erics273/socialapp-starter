de968d6dbec6c093be95c78b3d6fed74d402e4a1
Grimes <crameg@amazon.com>
2020-01-28 11:53:28 -0500

    accordion

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DisplayMessage from '../displayMessage/displayMessage'
+import DisplayLike from '../displayLike/DisplayLike';
 import BlueService from '../../blueService';
 
 import Accordion from 'react-bootstrap/Accordion'
@@ -48,24 +49,33 @@ class Message extends React.Component {
         if (this.state.dataMessages.messages) {
 
             let messageArray = [];
-            for(let i = 0; i < this.state.dataMessages.messages.length; i++){
-                // for(let j = 0; j < this.state.dataMessages.likes.length; j++){
+            let tempLikesArray = []
+            for (let i = 0; i < this.state.dataMessages.messages.length; i++) {
+                tempLikesArray = [];
+                for (let j = 0; j < this.state.dataMessages.messages[i].likes.length; j++) {
+                    tempLikesArray.push(
+                        <DisplayLike
+                            key={this.state.dataMessages.messages[i].likes[j].id}
+                            username={this.state.dataMessages.messages[i].likes[j].username}
+                            date={this.state.dataMessages.messages[i].likes[j].createdAt}
+                        />
+                    );
+                }
 
-                // }
-                console.log(this.state.dataMessages.messages[i].likes.length)
                 messageArray.push(
                     <DisplayMessage
-                            key={this.state.dataMessages.messages[i].id}
-                            message={this.state.dataMessages.messages[i].text}
-                            username={this.state.dataMessages.messages[i].username}
-                            date={this.state.dataMessages.messages[i].createdAt}
-                            likes={this.state.dataMessages.messages[i].likes.length}
-                        />
+                        key={this.state.dataMessages.messages[i].id}
+                        message={this.state.dataMessages.messages[i].text}
+                        username={this.state.dataMessages.messages[i].username}
+                        date={this.state.dataMessages.messages[i].createdAt}
+                        likes={this.state.dataMessages.messages[i].likes.length}
+                        likeArray={tempLikesArray}
+                    />
                 )
             }
 
             return (
-                <Accordion defaultActiveKey="0">
+                <Accordion >
                     {/* {this.state.dataMessages.messages.map((message)=>{
                         return <DisplayMessage
                             key={message.id}
