eab6460979dfce07bec391da2353f0135fe3ad21
Grimes <crameg@amazon.com>
2020-01-28 15:13:36 -0500

    user message redirect

--- src/blueService.js
+++ src/blueService.js
@@ -30,9 +30,9 @@ class BlueService {
     //   console.log(error)
     // });
 
-    getMessage() {
-        return this.client.get(this.url + 'messages/15');
-    }
+    // getMessage() {
+    //     return this.client.get(this.url + 'messages/15');
+    // }
 
     gitMessageSpecific(messageID) {
         return this.client.get(this.url + 'messages/' + messageID);
