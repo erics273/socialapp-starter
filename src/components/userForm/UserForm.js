cc4ce2e6069ff1d7a7ac66b0ad12fc4f4664f67a
Jones <seanjone@amazon.com>
2020-01-28 11:56:32 -0500

    start userform

---
+++ src/components/userForm/UserForm.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+// import { userIsNotAuthenticated } from "../../HOCs";
+import BlueService from '../../blueService';
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class UserForm extends Component {
+    constructor(props) {
+        super(props);
+        this.client = new BlueService();
+        this.state = {
+            messageError: "",
+            formMessage: {
+                message: ""
+            },
+        };
+    }
+
+
+    submitMessage = (event) => {
+        event.preventDefault();
+
+        console.log(this.state.formMessage.text);
+
+        this.client.postMessage(this.state.formMessage.text)
+            .then((response) => {
+                console.log(response)
+
+                this.getMessages();
+
+                this.setState({
+                    messageError: "",
+                    formMessage: {
+                        text: "",
+                    }
+                });
+            }).catch((error) => {
+                console.log(error)
+            });
+
+    }
+
+    submitMessageStrap = (event) => {
+        event.preventDefault();
+        
+        console.log(this.state.formMessage.message);
+
+        this.client.postMessage(this.state.formMessage.message)
+            .then((response) => {
+                console.log(response)
+
+                this.getMessages();
+
+                this.setState({
+                    messageError: "",
+                    formMessage: {
+                        text: "",
+                    }
+                });
+            }).catch((error) => {
+                console.log(error)
+            });
+    }
+
+    handleChangeMessageStrap = (event) => {
+        console.log(event.target.id);
+        console.log(event.target.value);
+        let formMessage = this.state.formMessage;
+        formMessage[event.target.id] = event.target.value;
+        this.setState({formMessage});
+    }
+
+    handleChangeMessage = (event) => {
+        let formMessage = this.state.formMessage;
+        formMessage[event.target.name] = event.target.value;
+        console.log(formMessage[event.target.name]);
+        this.setState({ formMessage });
+    }
+
+    render() {
+
+
+        return (
+            <Container>
+
+                <Form onSubmit={this.submitMessageStrap}>
+
+                    <Form.Group controlId="message">
+                        <Form.Label>Ink</Form.Label>
+                        <Form.Control onChange={this.handleChangeMessageStrap} placeholder="Ink" />
+                        <Form.Text className="text-muted">
+                            Spill Some Ink
+                            </Form.Text>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+
+                </Form>
+
+            </Container>
+        )
+    }
+
+
+
+}
+
+
+
+export default UserForm;
